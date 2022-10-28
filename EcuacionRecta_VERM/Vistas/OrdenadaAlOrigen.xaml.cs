a026086ac150c4b9b3699f7cecc9fcb798aff837
VeroCR <110310388+VeroCR@users.noreply.github.com>
2022-10-28 13:45:14 -0500

    Agregar archivos de proyecto.

---
+++ EcuacionRecta_VERM/Vistas/OrdenadaAlOrigen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EcuacionRecta_VERM.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrdenadaAlOrigen : ContentPage
+    {
+        public OrdenadaAlOrigen()
+        {
+            InitializeComponent();
+        }
+    }
+}
